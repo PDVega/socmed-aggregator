0a66acdf27043dd131de0cf9a1fdf9dfe06d3d05
pdvega <pdvegaa@gmail.com>
2017-08-02 23:28:29 +0700

    twitter search done

--- client/main.js
+++
@@ -1,9 +0,0 @@
-function performGetTimeline(){
-  var resultElement = document.getElementById('getTimeline');
-  resultElement.innerHTML = '';
-  
-  axios.get('http://localhost:3000/api/timeline')
-  .then(function (response){
-    resultElement.innerHTML = 
-  })
-}
